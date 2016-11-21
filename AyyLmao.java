7529719197a43f0cf7d8dbff6aded2bd1f43bf6a
tyron538 <aidenhong@live.com>
2016-11-20 18:55:29 -0800

    AYY LMAOOOOO

--- AyyLmao.java
+++ AyyLmao.java
@@ -1,13 +1,27 @@
 public class AyyLmao{
    public static void main (String[] args) {
-   System.out.println("        _ _ _ _              ");
-   System.out.println("       | | | | |              ");
-   System.out.println("       | | | | |_              ");
-   System.out.println("       | | | | | |             ");
-   System.out.println("       |       | |      ");
-   System.out.println("       \"        |              ");
-   System.out.println("       \"________/      ");
-   System.out.println("                   ");
+   System.out.println("       _________             ");
+   System.out.println("      /         \\      ");
+   System.out.println("     /           \\ ");
+   System.out.println("    |             |      ");
+   System.out.println("    |       _     | ");
+   System.out.println("     \\ .   | |   / ");
+   System.out.println("      \\__L_|_|__/        ");
+   System.out.println("   ///          \\\\\\   ");
+   System.out.println("  ///  _ _ _ _   \\\\\\            ");
+   System.out.println("      | | | | |_              ");
+   System.out.println("      |_|_|_|_| |                ");
+   System.out.println("      \\        /      ");
+   System.out.println("       \\______/        ");
+   System.out.println("       |      |        ");
+   System.out.println("       |      |        ");
+   System.out.println("       |      |        ");
+   System.out.println("       |      |        ");
+   System.out.println("       |      |        ");
+   System.out.println("       |      |        ");
+   System.out.println("       |      |        ");
+   
+
 
 
 
